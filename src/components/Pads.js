8eb3e7e24df97ecffd09a05660ef90c046421222
Dennis <deweis@gmx.ch>
2018-12-08 11:50:52 +0100

    Add the pads

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Wrapper from './Wrapper';
+import './Pads.css';
 
 const Pads = () => {
-  return <div>Pads</div>;
+  return (
+    <Wrapper>
+      <table className="centered">
+        <tbody>
+          <tr>
+            <td>Q</td>
+            <td>W</td>
+            <td>E</td>
+          </tr>
+          <tr>
+            <td>A</td>
+            <td>S</td>
+            <td>D</td>
+          </tr>
+          <tr>
+            <td>Z</td>
+            <td>X</td>
+            <td>C</td>
+          </tr>
+        </tbody>
+      </table>
+    </Wrapper>
+  );
 };
 
 export default Pads;
